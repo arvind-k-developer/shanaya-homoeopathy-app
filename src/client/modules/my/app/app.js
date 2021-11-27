657f7dd3b1ac4c7418155c0bc3bb5dbe28f160cf
sfdx4devops <arvind.k.developer@gmail.com>
2021-11-27 23:51:43 +0530

    deploying shanaya homoeopathy app

---
+++ src/client/modules/my/app/app.js
@@ -0,0 +1,72 @@
+import { LightningElement } from 'lwc';
+const defaultData = {
+    Name:'',
+    Email: '',
+    Mobile:'',
+    Address:''
+}
+
+const BASE_URL = 'http://localhost:3002'
+
+export default class App extends LightningElement {
+    //local property
+    formData = defaultData
+    loader = false
+    isSubmitted = false
+
+    //getter
+    get checkinMsg(){
+        return  `Welcome, ${this.formData.Name}. You're checked in!`
+    }
+
+    // method on form change
+    formchange(event){
+        const {name, value} = event.detail
+        this.formData = {...this.formData, [name]:value}
+    }
+    // method on submit
+    checkInHandler(event){
+        event.preventDefault()
+        this.loader = true
+        
+        this.formData={...this.formData,
+            "Date":new Date().toLocaleDateString(),
+            "Time":new Date().toLocaleTimeString()
+        }
+        console.log(this.formData)
+
+        fetch(`${BASE_URL}/api/v1/submit`, {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(this.formData)
+        }).then(response=>response.json())
+        .then(result=>{
+            if(result.success){
+               this.isSubmitted = true 
+               window.setTimeout(()=>{
+                   this.isSubmitted = false
+                   this.formData={...defaultData}
+               }, 6000)
+            }
+        })  
+
+        .catch(error=>console.error(error))
+        .finally(()=>{
+            this.loader = false
+        })
+    }
+    
+
+    //on load hook
+    // connectedCallback(){
+    //     this.fetchData()
+    // }
+    // fetchData(){
+    //     fetch(BASE_URL)
+    //     .then(response=>response.json())
+    //     .then(result=>console.log(result))
+    //     .catch(error=>console.error(error))
+    // }
+}
---
+++ src/server/api.js
@@ -0,0 +1,89 @@
+// Simple Express server setup to serve for local testing/dev API server
+const compression = require('compression');
+const helmet = require('helmet');
+const express = require('express');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+// loading env files
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+//extracting credentials from env
+const {SHEET_ID, CLIENT_EMAIL, PRIVATE_KEY} = process.env
+//console.log({SHEET_ID, CLIENT_EMAIL, PRIVATE_KEY})
+
+const app = express();
+app.use(helmet());
+app.use(compression());
+app.use(function(req, res, next){
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+})
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}))
+
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3002;
+const DIST_DIR = './dist';
+
+app.use(express.static(DIST_DIR));
+
+//google sheet function
+
+async function accessSpreadsheet(data){
+    try{
+        // Initialize the sheet - doc ID is the long id in the sheets URL
+        const doc = new GoogleSpreadsheet(SHEET_ID);
+        // Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+        await doc.useServiceAccountAuth({
+        client_email: CLIENT_EMAIL,
+        private_key: PRIVATE_KEY.replace(/\\n/gm, '\n')
+        });
+
+        await doc.loadInfo(); // loads document properties and worksheets
+        console.log(doc.title);
+        
+        //create a sheet and set the header row
+        const sheet = await doc.sheetsByIndex[0]
+        //append rows
+        return await sheet.addRow(data)
+    }catch(error){
+        console.error(error)
+    }
+}
+
+//accessSpreadsheet()
+
+app.get('/api/v1/test', (req, res) => {
+    res.json({ msg:"Api is running successfully", success: true });
+});
+
+app.get('/api/v1/sheetname',async (req, res) => {
+    const sheetName = await accessSpreadsheet()
+    res.json({ sheetName, success: true });
+});
+
+
+app.post('/api/v1/submit', async(req, res)=>{
+    console.log(req.body)
+    await accessSpreadsheet(req.body)
+    res.json({ msg:'Data recieved successfully!!', success: true });
+})
+
+app.listen(PORT, () =>
+    console.log(
+        `✅  App is running on: http://${HOST}:${PORT}`
+    )
+);
